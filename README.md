fc767e46401072d3f68cc4020396bf816c820699
Arnold Lei <arnold.lei88@gmail.com>
2016-06-06 16:57:21 -0400

    I can now search giphy for gifs at a click of a button

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 # Animaltastic
 
 ##Introduction
-This is my submission for the Animal Tastic assignment for Rutgers Coding Bootcamp
+This is my submission for the Animal Tastic assignment for Rutgers Coding Bootcamp. I will be coding this assingment from sctatch. 
+
 
--- assets/js/animaltastic.js
+++ assets/js/animaltastic.js
@@ -1,18 +1,42 @@
 $(document).ready(function(){
   var gifs; 
-
+  var animals = ['cats', 'dogs', 'racoons'];
   //html divs
   var div = $('.gifs');
-  $.ajax({
-    url:'http://api.giphy.com/v1/gifs/search?q=funny+cat&api_key=dc6zaTOxFJmzC',
-    method: 'GET',
-  }).done(function(gifObj){
-    console.log(gifObj.data)
-    for (var i = 0; i < gifObj.data.length; i++){
-      var newImg = $('<img>');
-      newImg.attr('src', gifObj.data[i].images.fixed_height.url);
-      div.append(newImg);
-      console.log(gifObj)
+  var buttons = $('.buttons');
+
+  function createButtons(){
+    for (var i = 0; i < animals.length; i++){
+      var newButton = $('<button>');
+      newButton.attr('class', 'btn btn-primary animalSearch').val(animals[i]).text(animals[i]);
+      console.log(animals[i]);
+      buttons.append(newButton);
     }
-  })
+  }
+  function buttonClick(){
+    $('.animalSearch').on('click', function(){
+      search(this.value)
+    })
+  }
+
+  function search(term){
+    $.ajax({
+      url:'http://api.giphy.com/v1/gifs/search?q='+term+'&api_key=dc6zaTOxFJmzC',
+      method: 'GET',
+    }).done(function(gifObj){
+      div.empty();
+      for (var i = 0; i < gifObj.data.length; i++){
+        var newImg = $('<img>');
+        var slug = $('<p>');
+        slug.text(gifObj.data[i].slug)
+        newImg.attr('src', gifObj.data[i].images.fixed_height.url);
+        div.append(newImg);
+        console.log(gifObj)
+      }
+    })
+  }
+
+  createButtons();
+  buttonClick();
+
 }); 
